f41fe30756ee733a59cd1cb39135f4efaf2a7eb4
TrentonPratt <trenp95@gmail.com>
2023-08-30 14:11:10 -0400

    Completed exercise 12

---
+++ SQL/Complete SQL Mastery - Code With Mosh/Exercises/Retrieving Data From Multiple Tables/exercise12.sql
@@ -0,0 +1,20 @@
+-- Exercise 11
+
+-- Use create-db-invoicing.sql
+
+-- Join payments table with payment_methods and clients
+--  Produce report with name of client and payment method
+
+USE invoicing;
+
+SELECT
+	p.invoice_id,
+	p.date,
+	p.amount,
+    c.name,
+    pm.name
+FROM payments p 
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
+JOIN clients c 
+	ON p.client_id = c.client_id
