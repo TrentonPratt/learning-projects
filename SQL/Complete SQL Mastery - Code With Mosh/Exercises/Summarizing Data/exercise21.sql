f2a0c2bc4e9508a8485beb08105908c9b451da2d
TrentonPratt <trenp95@gmail.com>
2023-09-11 14:54:52 -0400

    Completed exercise 21

---
+++ SQL/Complete SQL Mastery - Code With Mosh/Exercises/Summarizing Data/exercise21.sql
@@ -0,0 +1,17 @@
+-- Exercise 20
+
+-- Use create-db-invoicing.sql
+
+-- Return table grouping by date, payment_method, and total_payments
+
+USE invoicing;
+
+SELECT
+	p.date,
+    pm.name AS payment_method,
+    SUM(p.amount) AS total_payments
+FROM payments p
+JOIN payment_methods pm 
+	ON p.payment_method = pm.payment_method_id
+GROUP BY date, payment_method
+ORDER BY date
