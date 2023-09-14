6a7acc75e5c330fb960f30d241576bdfb0362979
TrentonPratt <trenp95@gmail.com>
2023-09-14 12:22:51 -0400

    Completed exercise 22

---
+++ SQL/Complete SQL Mastery - Code With Mosh/Exercises/Summarizing Data/exercise22.sql
@@ -0,0 +1,26 @@
+-- Exercise 20
+
+-- Use create-db-store.sql
+
+-- Get the customers
+--  located in Virginia
+--  who have spent more than $100
+
+USE store;
+
+SELECT
+	c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.state,
+    SUM(oi.quantity * oi.unit_price) AS total_spent
+FROM customers c 
+JOIN orders o USING (customer_id)
+JOIN order_items oi USING (order_id)
+WHERE state = 'VA'
+GROUP BY 
+	c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.state
+HAVING total_spent > 100
