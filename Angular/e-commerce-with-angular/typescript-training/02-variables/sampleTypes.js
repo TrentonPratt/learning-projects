46c35353d6c456d1841ac173516a99b79c3ef220
trenton.pratt <trenton.pratt@kinsaleins.com>
2024-07-30 09:46:37 -0400

    Completed types lesson

---
+++ Angular/e-commerce-with-angular/typescript-training/02-variables/sampleTypes.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Trenton";
+var lastName = "Pratt";
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi ".concat(firstName, " ").concat(lastName));
---
+++ Angular/e-commerce-with-angular/typescript-training/02-variables/sampleTypes.ts
@@ -0,0 +1,8 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Trenton";
+let lastName: string = "Pratt";
+
+console.log(found);
+console.log("The grade is " + grade);
+console.log(`Hi ${firstName} ${lastName}`)
